a88216f0ffc62ffce27229022cc2f472a0f0affc
aadiityaasingh <adityasingh.6488@gmail.com>
2026-06-24 18:14:48 +0530

    making changes according to the use

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,14 @@ import Login from "./pages/auth/Login";
 import Dashboard from "./pages/dashboard/Dashboard";
 
 import ProtectedRoute from "./routes/ProtectedRoute";
+import PatientList from "./pages/patients/PatientList";
+import TherapyList from "./pages/therapies/TherapyList";
+import TherapyPlanList from "./pages/plans/TherapyPlanList";
+import SessionList from "./pages/sessions/SessionList";
+import BillList from "./pages/billing/BillList";
+import AddPatient from "./pages/patients/AddPatient";
+import PatientHistory from "./pages/patients/PatientHistory";
+import AddTherapy from "./pages/therapies/AddTherapy";
 
 const App = () => {
   return (
@@ -12,14 +20,16 @@ const App = () => {
 
       <Route path="/login" element={<Login />} />
 
-      <Route
-        path="/dashboard"
-        element={
-          <ProtectedRoute>
-            <Dashboard />
-          </ProtectedRoute>
-        }
-      />
+      <Route path="/dashboard" element={<Dashboard />} />
+
+      <Route path="/patients" element={<PatientList />} />
+      <Route path="/therapies" element={<TherapyList />} />
+      <Route path="/plans" element={<TherapyPlanList />} />
+      <Route path="/sessions" element={<SessionList />} />
+      <Route path="/bills" element={<BillList />} />
+      <Route path="/patients/add" element={<AddPatient />} />
+      <Route path="/patients/:id" element={<PatientHistory />} />
+      <Route path="/therapies/add" element={<AddTherapy />} />
     </Routes>
   );
 };
