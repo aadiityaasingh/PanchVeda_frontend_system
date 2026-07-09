feebd8c46fd28ec63eb7f4dab957ed10fd2d5ced
aadiityaasingh <adityasingh.6488@gmail.com>
2026-07-09 19:03:11 +0530

    for api calling things

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,17 +1,25 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
-axiosInstance.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
+// attach token automatically
 
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
 
-  return config;
-});
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+
+  (error) => {
+    return Promise.reject(error);
+  },
+);
 
 export default axiosInstance;
