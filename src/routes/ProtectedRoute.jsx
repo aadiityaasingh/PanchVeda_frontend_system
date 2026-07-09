3fd36c34569e6dfc344b4c96be28764c31caf353
aadiityaasingh <adityasingh.6488@gmail.com>
2026-07-09 19:02:25 +0530

    for protecting from unauthorized user

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -1,10 +1,19 @@
 import { Navigate } from "react-router-dom";
+
 import { useAuth } from "../context/AuthContext";
 
-const ProtectedRoute = ({ children }) => {
-  const { isAuthenticated } = useAuth();
+const ProtectedRoute = ({ children, roles }) => {
+  const { user } = useAuth();
 
-  return isAuthenticated ? children : <Navigate to="/login" />;
+  if (!user) {
+    return <Navigate to="/login" />;
+  }
+
+  if (roles && !roles.includes(user.role)) {
+    return <Navigate to="/unauthorized" />;
+  }
+
+  return children;
 };
 
 export default ProtectedRoute;
